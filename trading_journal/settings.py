6be801cc0c379f0fe1b153e9934ff815c13acb1a
Wiqi <arya.banerjee8627@gmail.com>
2026-01-21 18:57:42 +0530

    Update settings for Render deployment

--- trading_journal/settings.py
+++ trading_journal/settings.py
@@ -19,16 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9#1v3!1qmpdko#669m^83&ka327-)d94x86hlu*1g_i$rspsbh"
+import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-9#1v3!1qmpdko#669m^83&ka327-)d94x86hlu*1g_i$rspsbh")
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "*")]
+
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://trading-journal-hnu3.onrender.com"]
+    "https://trading-journal-tw4t.onrender.com"
+]
+
 
 # Application definition
 
