0154dfcb293478b1f39781a8c074382c51e984e4
Wiqi <arya.banerjee8627@gmail.com>
2026-01-21 18:07:00 +0530

    Fix login URL for live deployment

--- trading_journal/settings.py
+++ trading_journal/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'  # after login, go to homepage
+LOGOUT_REDIRECT_URL = '/login/'  # after logout
